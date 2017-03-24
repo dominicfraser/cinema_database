77bc80e1ad6c19ec34556b657d997297841c939b
Dominic <mrdjfraser@gmail.com>
2017-03-24 15:16:29 +0000

    params and save method set up for customer

--- models/customer.rb
+++ models/customer.rb
@@ -2,11 +2,18 @@ require_relative('../db/SqlRunner')
 
 class Customer
 
-  attr_reader
+  attr_reader :id
 
-  def initialize()
-
+  def initialize(customer_hash)
+    @id = customer_hash['id'].to_i
+    @name = customer_hash['name']
+    @category = customer_hash['category'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO customers (name, category) VALUES ('#{name}', #{category} RETURNING *"
+    customer = SqlRunner.run(sql)
+    @id = customer['id'].to_i
+  end
 
 end
